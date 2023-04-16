93eb4e66c662ac1417b2b7b69a327b8ff0d9b310
Jędrzej Głowaczewski <jedrzej.glowaczewski@gmail.com>
2023-04-16 12:44:39 +0200

    Add cube with script syncing position with a phone

--- AAR/Assets/Scripts/SyncPoseReceiver.cs
+++ AAR/Assets/Scripts/SyncPoseReceiver.cs
@@ -22,7 +22,7 @@ public class SyncPoseReceiver : SyncPose
     protected override void Start()
     {
         base.Start();
-
+        Debug.Log("SyncPoseReceiver: Starting the receiver");
         // To be able to receive broadcast messages we have to specify broadcast credentials
         NetworkTransport.SetBroadcastCredentials(hostID, BROADCAST_CREDENTIALS_KEY, BROADCAST_CREDENTIALS_VERSION,
             BROADCAST_CREDENTIALS_SUBVERSION, out byte error);
@@ -46,18 +46,22 @@ public class SyncPoseReceiver : SyncPose
                     // Nothing has happend. That's a good thing :-)
                     break;
                 case NetworkEventType.BroadcastEvent:
+                    Debug.Log($"SyncPoseReceiver: Received a broadcast message");
                     string address = GetIPAddress(outHostID);
                     if (address != null)
                         ConnectToTheServer(address);
                     break;
                 case NetworkEventType.ConnectEvent:
+                    Debug.Log($"SyncPoseReceiver: Connected to the server");
                     connectionEstablished.Invoke();
                     break;
                 case NetworkEventType.DataEvent:
+                    Debug.Log($"SyncPoseReceiver: Received a new pose");
                     Pose p = updateTransform ? UpdateTransform() : DeserializePose(messageBuffer, formatter);
                     newPoseReceived.Invoke(p.position, p.rotation);
                     break;
                 case NetworkEventType.DisconnectEvent:
+                    Debug.Log($"SyncPoseReceiver: Disconnected from the server");
                     connectionLost.Invoke();
                     break;
             }
@@ -68,7 +72,7 @@ public class SyncPoseReceiver : SyncPose
     protected virtual Pose UpdateTransform()
     {
         Pose receivedPose = DeserializePose(messageBuffer, formatter);
-
+        Debug.Log($"SyncPoseReceiver: Received a new pose: {receivedPose}");
         transform.position = receivedPose.position;
         transform.rotation = receivedPose.rotation;
 
@@ -77,6 +81,7 @@ public class SyncPoseReceiver : SyncPose
 
     protected virtual int ConnectToTheServer(string address)
     {
+        Debug.Log($"SyncPoseReceiver: Connecting to the server at {address}");
         сonnectionID = NetworkTransport.Connect(hostID, address, port, 0, out byte error);
         if ((NetworkError)error != NetworkError.Ok)
         {
@@ -89,6 +94,7 @@ public class SyncPoseReceiver : SyncPose
 
     protected static string GetIPAddress(int hostID)
     {
+        Debug.Log($"SyncPoseReceiver: Getting an IP address {hostID}");
         NetworkTransport.GetBroadcastConnectionInfo(hostID, out string address, out int port, out byte error);
         if ((NetworkError)error != NetworkError.Ok)
         {
@@ -101,6 +107,7 @@ public class SyncPoseReceiver : SyncPose
 
     protected static Pose DeserializePose(byte[] buffer, BinaryFormatter formatter)
     {
+        Debug.Log($"SyncPoseReceiver: Deserializing a pose");
         using (var stream = new MemoryStream(buffer))
         {
             return (Pose)formatter.Deserialize(stream);
