6a05b9108b310d824db2a5e13839bfb53e9e0668
anna-gorska <aniajj9@gmail.com>
2023-04-30 10:39:17 +0200

    instructions second draft - 2 diff panels for 2 phases

--- AAR/Assets/Scripts/InstructionsPanel1.cs
+++ AAR/Assets/Scripts/InstructionsPanel1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InstructionsPanel : MonoBehaviour
+public class InstructionsPanel1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,7 +13,7 @@ public class InstructionsPanel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (SyncPoseReceiver.synchronizationStage >= 1){
+        if (SyncPoseReceiver.synchronizationStage >= 2){
             this.gameObject.SetActive(false);
         }
         else{
